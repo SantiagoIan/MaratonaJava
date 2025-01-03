132236633a00308ca40fdd5444c8860487e9abc0
TesteGit <santiagooiian@gmail.com>
2025-01-02 23:13:08 -0300

    fix

--- src/Maratonajava/devdojo/introducao/Operadores01.java
+++ src/Maratonajava/devdojo/introducao/Operadores01.java
@@ -1,21 +1,20 @@
 package Maratonajava.devdojo.introducao;
 
-import java.util.Scanner;
 
 public class Operadores01 {
     public static void main(String[] args) {
-        Scanner dados = new Scanner(System.in);
-        System.out.print("Digite um numero: ");
-        int num = dados.nextInt();
 
-        System.out.println("Digite outro número: ");
-        int num2 = dados.nextInt();
+        int num = 10;
+        int num2 = 2;
 
-        boolean maior = num > num2;
+        int div = num / num2;
+        int multi = num * num2;
+
+        System.out.println(++num); // 10 + 1
+        System.out.println(--num2); // 2 - 1
+        System.out.println(div); // 10 / 2
+        System.out.println(multi); // 10 * 2
 
 
-        System.out.println("Seu número mais 1: " + ++num);
-        System.out.println("Seu número menos 1: " + --num);
-        System.out.println(num + " é maior que " + num2 + " ? " + maior);
     }
 }
