0f9e7a7b7b1caa5fd73722ea908d39ef4df612f3
TesteGit <santiagooiian@gmail.com>
2025-01-15 21:41:02 -0300

    Sobrescrita toString - 76

---
+++ src/Maratonajava/devdojo/javacore/Isobreescrita/Teste/AnimeTeste01.java
@@ -0,0 +1,11 @@
+package Maratonajava.devdojo.javacore.Isobreescrita.Teste;
+
+import Maratonajava.devdojo.javacore.Isobreescrita.Dominio.Anime;
+
+public class AnimeTeste01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Black Clover");
+
+        System.out.println(anime);
+    }
+}
