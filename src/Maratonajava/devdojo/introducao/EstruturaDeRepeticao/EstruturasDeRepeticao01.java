41694d6ba9ea94a8eb043375c5a5adebf9cf2318
TesteGit <santiagooiian@gmail.com>
2025-01-24 16:28:17 -0300

    FIX

--- src/Maratonajava/devdojo/introducao/EstruturaDeRepeticao/EstruturasDeRepeticao01.java
+++ src/Maratonajava/devdojo/introducao/EstruturaDeRepeticao/EstruturasDeRepeticao01.java
@@ -7,17 +7,19 @@ public class EstruturasDeRepeticao01 {
         int count = 0;
 
         while (count <= 10) {
-            System.out.println(count++);
+            System.out.println("Dentro do While: " + count++);
         }
 
         count = 0;
-        do{
-            System.out.println("Dentro do do-while: "+ ++count);
-        } while (count <10);
 
-        for (int i = 0; i <= 10; i++) {
-            System.out.println("Dentro do For: "+i);
+        do {
+            System.out.println("Dentro do do-while: " + ++count);
+        } while (count < 10);
 
+        for (int i = 0; i <= 10; i--) {
+            System.out.println("Dentro do For: " + i);
         }
+
+
     }
 }
