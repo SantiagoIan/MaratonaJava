ab004e340728e27a58b5c548df5f1b07e878ef46
Ian <101881756+SantiagoIan@users.noreply.github.com>
2025-04-28 12:35:20 -0300

    Classes Utilitárias - String 108

--- src/Maratonajava/devdojo/javacore/Qstring/Teste/StringTeste01.java
+++ src/Maratonajava/devdojo/javacore/Qstring/Teste/StringTeste01.java
@@ -1,4 +1,16 @@
 package Maratonajava.devdojo.javacore.Qstring.Teste;
 
 public class StringTeste01 {
+    public static void main(String[] args) {
+        String nome = "Santiago"; // String Constant pool
+        String nome2 = "Santiago";
+        nome = nome.concat(" Ian"); // Não altera o valor de nome, pois String é imutável
+        System.out.println(nome);
+        System.out.println(nome == nome2); // false
+
+        String nome3 = new String("Santiago"); // String Heap (não é uma String literal)
+        System.out.println(nome2 == nome3); // false
+        System.out.println(nome2 == nome3.intern()); // true
+
+    }
 }
