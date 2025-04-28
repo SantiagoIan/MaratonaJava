bc6791af955acccd98d2fed5e45538521ca56a27
Ian <101881756+SantiagoIan@users.noreply.github.com>
2025-04-28 12:49:15 -0300

    Classes Utilitárias - String 108

--- src/Maratonajava/devdojo/javacore/Qstring/Teste/StringTeste01.java
+++ src/Maratonajava/devdojo/javacore/Qstring/Teste/StringTeste01.java
@@ -6,11 +6,11 @@ public class StringTeste01 {
         String nome2 = "Santiago";
         nome = nome.concat(" Ian"); // Não altera o valor de nome, pois String é imutável
         System.out.println(nome);
-        System.out.println(nome == nome2); // false
+        System.out.println(nome == nome2); // false (== usado para comparar referências)
 
         String nome3 = new String("Santiago"); // String Heap (não é uma String literal)
         System.out.println(nome2 == nome3); // false
-        System.out.println(nome2 == nome3.intern()); // true
+        System.out.println(nome2 == nome3.intern()); // true (intern() retorna a referência da String no pool de Strings)
 
     }
 }
