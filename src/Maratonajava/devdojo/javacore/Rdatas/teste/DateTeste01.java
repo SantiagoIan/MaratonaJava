865daf4eeb73caf0a0e5b6586d9e9a066c7589a0
Ian <101881756+SantiagoIan@users.noreply.github.com>
2025-05-01 17:50:48 -0300

    Classes Utilitárias (Date) - 112

---
+++ src/Maratonajava/devdojo/javacore/Rdatas/teste/DateTeste01.java
@@ -0,0 +1,11 @@
+package Maratonajava.devdojo.javacore.Rdatas.teste;
+
+import java.util.Date;
+
+public class DateTeste01 {
+    public static void main(String[] args) {
+        Date date = new Date(); // Representa um long, desde 1970 até a data atual
+        // todos os metodos da classe Date estão depreciados.
+        System.out.println(date);
+    }
+}
