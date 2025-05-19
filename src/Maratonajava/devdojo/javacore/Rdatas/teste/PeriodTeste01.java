8c9cd2de80d4bea82c1db1c4e1cf781d84501f15
Ian <101881756+SantiagoIan@users.noreply.github.com>
2025-05-18 21:28:26 -0300

    Classes Utilitárias (Period) - 124

--- src/Maratonajava/devdojo/javacore/Rdatas/teste/PeriodTeste01.java
+++ src/Maratonajava/devdojo/javacore/Rdatas/teste/PeriodTeste01.java
@@ -3,6 +3,7 @@ package Maratonajava.devdojo.javacore.Rdatas.teste;
 import java.time.LocalDate;
 
 import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 public class PeriodTeste01 {
     public static void main(String[] args) {
@@ -10,11 +11,14 @@ public class PeriodTeste01 {
         LocalDate nowAfter2Years = LocalDate.now().plusYears(2);
         Period period1 = Period.between(now, nowAfter2Years);
         Period period2 = Period.ofDays(10);
-        Period period3 = Period.ofWeeks(5); // Period, nao trabalha com semanas
+        Period period3 = Period.ofWeeks(48); // Period, nao trabalha com semanas
         Period period4 = Period.ofMonths(3);
         System.out.println(period1);
         System.out.println(period2);
         System.out.println(period3);
         System.out.println(period4);
+        System.out.println();
+
+        System.out.println(now.until(now.plusDays(period3.getDays()), ChronoUnit.MONTHS));
     }
 }
